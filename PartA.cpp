30e3969c3c10b01dc8146b4a949f5176b5fb0081
EddySupra <70074613+EddySupra@users.noreply.github.com>
2022-04-09 16:27:12 -0700

    Update PartA.cpp
    prints the correct output just need to add in user inputs for arrays

--- PartA.cpp
+++ PartA.cpp
@@ -22,7 +22,9 @@ void target_subarray(std::string a[], std::string b[])
 {
     // Arrays for the answers
     std::size_t a1[4];
-    int b1[4];
+    std::string b1[4];
+    
+    std::map<std::size_t,std::string> first;
     
     std::string cities = a[0];
     int cities_length = cities.length();
@@ -37,17 +39,24 @@ void target_subarray(std::string a[], std::string b[])
         //get the city from array and give the string a variable
         std::string city = b[i];
         std::size_t found = cities.find(city);
+
         if(found != std::string::npos)
         {
             a1[i] = found;
+            b1[i] = city;
+            first[a1[i]] = b1[i];
         }
         //std::string::find(cities.begin(), cities.end(), city);
         
     }
+    std::sort(a1, a1 + 4);
+    
+   
+    
     
     for(int i = 0; i < 4; i++)
     {
-        std::cout << a1[i];
+        std::cout << a1[i] << " " << first[a1[i]] << std::endl;
     }
 
 }
